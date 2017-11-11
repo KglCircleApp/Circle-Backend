d00d2ca211935372a281dcc407bb957b3adfb529
kush5900 <nsanzudaniel59@gmail.com>
2017-11-11 22:43:10 +0200

    fuckkk

--- db/passport.js
+++ db/passport.js
@@ -1,15 +1,14 @@
 const JwtStrategy = require('passport-jwt').Strategy;
 const ExtractJwt = require('passport-jwt').ExtractJwt;
 const User = require('../models/user');
-const config = require('../db/database');
+const config = require('./database');
 
 module.exports = function(passport){
  let opts ={};
- opts.jwtFromRequest = ExtractJwt.fromAuthHeader();
+ opts.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
  opts.secretOrKey = config.secret;
- passport.use(new JwtStrategy(opts,(jwt_payload,done)=>{
-   console.log(jwt_payload);
-   User.getUserById(jwt_payload._doc._id,(err,user)=>{
+ passport.use(new JwtStrategy(opts,function(jwt_payload,done){
+   User.getUserById(jwt_payload._doc._id,function(err,user){
       if(err){
          return done(err,false);
       }if(user){
